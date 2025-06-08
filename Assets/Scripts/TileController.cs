9b25c804a689cb92234291852255bbaeba6179b0
unityzaidimai1223 <unityzaidimainkkm@gmail.com>
2025-06-08 16:54:55 +0300

    Initial commit

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileController : MonoBehaviour
+{
+    [Header("Component References")]
+    public GameStateController gameController;                       
+    public Button interactiveButton;                                 
+    public Text internalText;                                        
+
+
+
+    
+    public void UpdateTile()
+    {
+        internalText.text = gameController.GetPlayersTurn();
+        interactiveButton.image.sprite = gameController.GetPlayerSprite();
+        interactiveButton.interactable = false;
+        gameController.EndTurn();
+    }
+
+    
+    public void ResetTile()
+    {
+        internalText.text = "";
+        interactiveButton.image.sprite = gameController.tileEmpty;
+    }
+}
